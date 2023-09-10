d27b38731c138a69014c4d66f08369b62a4f5515
edriso <edriso.dev@gmail.com>
2023-09-10 12:53:19 +0300

    change res.json structure in login

--- controllers/auth.js
+++ controllers/auth.js
@@ -32,7 +32,7 @@ const login = async (req, res) => {
   }
   // compare password
   const token = user.createJWT();
-  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
+  res.status(StatusCodes.OK).json({ user: { name: user.name, token } });
 };
 
 module.exports = {
