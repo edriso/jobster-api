368ed1a6f677f76ff44101cb2fadfa3b061dd94f
edriso <edriso.dev@gmail.com>
2023-09-13 12:42:58 +0300

    limit api for specific routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,19 @@
 const express = require("express");
-const router = express.Router();
+const { rateLimit } = require("express-rate-limit");
 const { register, login, updateUser } = require("../controllers/auth");
 const authenticateUser = require("../middleware/authentication");
 const testUser = require("../middleware/testUser");
+const router = express.Router();
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100, // Limit each IP to 100 requests per 15 minutes
+  message:
+    "Too many accounts created from this IP, please try again after 15 minutes",
+  standardHeaders: "draft-7",
+});
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", apiLimiter, register);
+router.post("/login", apiLimiter, login);
 router.patch("/updateUser", authenticateUser, testUser, updateUser);
 
 module.exports = router;
