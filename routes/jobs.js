0b38cd9f287ea99113d77587b787c6b6afc027c7
edriso <edriso.dev@gmail.com>
2023-09-14 23:07:16 +0300

    add stats response

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const testUser = require("../middleware/testUser");
 const {
-  createJob,
-  deleteJob,
   getAllJobs,
-  updateJob,
   getJob,
+  createJob,
+  updateJob,
+  deleteJob,
+  showStats,
 } = require("../controllers/jobs");
 const router = express.Router();
 
 router.route("/").post(testUser, createJob).get(getAllJobs);
-
+router.route("/stats").get(showStats);
 router
   .route("/:id")
   .get(getJob)
